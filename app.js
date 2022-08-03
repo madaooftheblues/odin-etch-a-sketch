9670255eee90fd525c62e56700b8641e994a96cf
madaooftheblues <azlanrasool@gmail.com>
2022-08-03 16:47:09 +0500

    Add styles

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+const CANVAS_COLOR = "beige";
+
 const canvas = document.querySelector(".canvas");
 const colorWell = document.querySelector("#colorWell");
+const eraser = document.querySelector("#eraser");
+const slider = document.querySelector("#slider");
+const pixels = document.querySelector("#pixels");
+const random = document.querySelector("#random");
+const clear = document.querySelector("#clear");
+const toolkit = document.querySelector(".toolkit");
 
 let color = "black";
+let mode = "color";
 
 function grid(n) {
   canvas.style.gridTemplateColumns = `repeat(${n},1fr)`;
@@ -12,11 +21,19 @@ function grid(n) {
   }
 }
 
+function select(e) {
+  return e.target.classList.add("selected");
+}
+
 function etch(e) {
   return (e.target.style.backgroundColor = color);
 }
 
 colorWell.addEventListener("input", (e) => (color = e.target.value));
+colorWell.addEventListener("click", (e) => {
+  select(e);
+  return (color = e.target.value);
+});
 canvas.addEventListener("mousedown", etch);
 canvas.addEventListener("mousedown", () =>
   canvas.addEventListener("mouseover", etch)
@@ -26,4 +43,18 @@ canvas.addEventListener("mouseup", () =>
   canvas.removeEventListener("mouseover", etch)
 );
 
-grid(64);
+eraser.addEventListener("click", () => (color = CANVAS_COLOR));
+clear.addEventListener("click", () => {
+  const squares = canvas.querySelectorAll("div");
+  squares.forEach((square) => (square.style.backgroundColor = CANVAS_COLOR));
+});
+
+slider.addEventListener("input", (e) => {
+  canvas.textContent = "";
+  pixels.textContent = `${e.target.value} x ${e.target.value}`;
+  return grid(e.target.value);
+});
+
+random.addEventListener("click", () => (randomClicked = true));
+
+grid(16);
